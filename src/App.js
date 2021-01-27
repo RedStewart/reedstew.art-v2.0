820a3c544ed1c7933e062ff63e11cf4560d7ca33
RedStewart <reedostewart@gmail.com>
2021-01-27 20:57:00 +1300

    Added shadow and text

--- src/App.js
+++ src/App.js
@@ -4,7 +4,20 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <p>reedstew.art</p>
+        <div className="floating" style={{ zIndex: "5" }}>
+          <p style={{ cursor: "pointer", opacity: "1" }}>
+            <a
+              href="https://github.com/RedStewart"
+              target="blank"
+              style={{ color: "inherit", textDecoration: "none" }}
+            >
+              reedstew.art
+            </a>
+          </p>
+        </div>
+        <div style={{ height: "100%", width: "100%" }}>
+          <p className="text-shadow">reedstew.art</p>
+        </div>
       </header>
     </div>
   );
